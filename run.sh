17afc3b56ddccd75ba1078edf3bd221363499d08
pumpkin blade <pumpkinblade@outlook.com>
2023-06-19 18:14:54 +0800

    resolve to make in linux

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-cmake -B build -DCMAKE_TOOLCHAIN_FILE=C:/DevTools/vcpkg/scripts/buildsystem/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows
+cmake -B build
 cmake --build build
 ./build/Debug/replace.exe --lef ./test/ispd18_test1.input.lef --def ./test/ispd18_test1.input.def
