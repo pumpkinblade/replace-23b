55ff9b4a1a9c33cb0382ec0ac8415e4006c48f15
zjl199904 <zjl199904@mail.ustc.edu.cn>
2023-07-11 05:51:10 +0000

    add a run script for gnu toolchain

---
+++ run-gnu.sh
@@ -0,0 +1,4 @@
+cmake -B build
+cmake --build build
+./build/replace --lef ./test/ispd18_test1.input.lef --def ./test/ispd18_test1.input.def
+
--- run-vcpkg.sh
+++ run-vcpkg.sh
@@ -1,3 +1,4 @@
 cmake -B build -DCMAKE_TOOLCHAIN_FILE=C:/DevTools/vcpkg/scripts/buildsystem/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows
 cmake --build build
 ./build/Debug/replace.exe --lef ./test/ispd18_test1.input.lef --def ./test/ispd18_test1.input.def
+
