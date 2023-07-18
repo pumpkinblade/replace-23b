0a05a4083ee9d3660b607e2b698bbf22c3204c0d
parker luxu <503682014@qq.com>
2023-07-18 14:07:37 +0000

    merge

--- src/partitioner.cpp
+++ src/partitioner.cpp
@@ -8,40 +8,40 @@
 namespace replace{
 
     std::shared_ptr<PlacerBase> P23bToBaseConverter::placer23bToPlaceBase(std::shared_ptr<Placer23b> placer23b_){
-        auto pb = std::make_shared<PlacerBase>();
-        Technology* topDieTechnology = placer23b_->topDieTechnology();
-        // Process die and rows
-        pb->dieStor_.emplace_back(placer23b_->topDie());
-        pb->dies_.push_back(&pb->dieStor_.back());
+        // auto pb = std::make_shared<PlacerBase>();
+        // Technology* topDieTechnology = placer23b_->topDieTechnology();
+        // // Process die and rows
+        // pb->dieStor_.emplace_back(placer23b_->topDie());
+        // pb->dies_.push_back(&pb->dieStor_.back());
         
-        // 给placebase预留空间
-        LOG_TRACE("Process def component");
-        std::unordered_map<std::string, int> instExtIds;
-        pb->instStor_.reserve(placer23b_->insts().size());
-        for (auto &inst : placer23b_->insts())
-        {
-            // 在Technology中找到对应的LibCell
-            auto libCell = topDieTechnology->cell(inst->libCellName());
-            // 将LibCell转换为Instance
-            Instance instance;
-            instance.setSize(libCell->sizeX(), libCell->sizeY());
-            bool isMacro = libCell->isMacro();
-            instance.setMacro(isMacro);
-            instance.setFixed(0);
-            // 将Instance加入到PlacerBase中
-            pb->instStor_.push_back(instance);
+        // // 给placebase预留空间
+        // LOG_TRACE("Process def component");
+        // std::unordered_map<std::string, int> instExtIds;
+        // pb->instStor_.reserve(placer23b_->insts().size());
+        // for (auto &inst : placer23b_->insts())
+        // {
+        //     // 在Technology中找到对应的LibCell
+        //     auto libCell = topDieTechnology->cell(inst->libCellName());
+        //     // 将LibCell转换为Instance
+        //     Instance instance;
+        //     instance.setSize(libCell->sizeX(), libCell->sizeY());
+        //     bool isMacro = libCell->isMacro();
+        //     instance.setMacro(isMacro);
+        //     instance.setFixed(0);
+        //     // 将Instance加入到PlacerBase中
+        //     pb->instStor_.push_back(instance);
             
-        }
-        LOG_TRACE("Process def net");
-        // Process def net
-        pb->netStor_.reserve(placer23b_->nets().size());
-        for (auto &net : placer23b_->nets()){
-            pb->netStor_.emplace_back();
-            for(auto &pin23b : net->pins()){
-                Pin pin;
-                // pin.setInstance(pin23b->instName());
-            }
-        }
+        // }
+        // LOG_TRACE("Process def net");
+        // // Process def net
+        // pb->netStor_.reserve(placer23b_->nets().size());
+        // for (auto &net : placer23b_->nets()){
+        //     pb->netStor_.emplace_back();
+        //     for(auto &pin23b : net->pins()){
+        //         Pin pin;
+        //         // pin.setInstance(pin23b->instName());
+        //     }
+        // }
 
     }
 
@@ -57,7 +57,7 @@ namespace replace{
         replace_->doInitialPlace();
         replace_->doNesterovPlace();
         // 对replace布局后的结果进行layer assignment
-        
+
 
     }
 
